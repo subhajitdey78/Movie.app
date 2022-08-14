5a33b626dfcd20814ef769ce8e2cd1b1511c7761
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2022-08-14 23:41:37 +0530

    first commit

---
+++ src/container/HomePage/HomePage.js
@@ -0,0 +1,24 @@
+
+import Navbar from "../../components/Navbar";
+import Banner from "../../components/Banner/Banner";
+import Row from "../../components/Row/Row";
+
+function HomePage() {
+     
+  return (
+    <div>
+        <Navbar />
+        
+
+        
+        <Banner/>
+        <Row title = "BOX OFFICE HITS" isLargeRow />
+        <Row title = "BOX OFFICE HITS" />
+        <Row title = "BOX OFFICE HITS" />
+        <Row title = "BOX OFFICE HITS" />
+        <Row title = "BOX OFFICE HITS" />
+            </div>
+  );
+}
+
+export default HomePage;
